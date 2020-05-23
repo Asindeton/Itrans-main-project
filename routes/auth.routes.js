f50057d980de7818330427008408953f9f161326
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2020-05-23 17:21:56 +0300

    feat: init project

---
+++ routes/auth.routes.js
@@ -0,0 +1,35 @@
+const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
+
+const router = Router()
+
+
+router.post('/register', async (req, res)=>{
+    try{
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email})
+
+        if(candidate){
+            return res.status(400).json({message: 'Такой ползователь уже существует'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({email, password: hashedPassword})
+
+        await user.save()
+        
+        res.status(201).json({message:'Пользователь создан'})
+
+    } catch(e){
+        res.status(500).json({ message: "Всё сломалось."})
+    }
+})
+
+
+router.post('/login', async (req, res)=>{
+
+})
+
+module.exports = router
